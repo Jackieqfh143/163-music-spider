95066c0beb060ec0167f0f9d7947960fa6e2046b
RainbomSea <1933422976@qq.com>
2019-01-29 11:15:10 +0800

    删除其他文件

--- Java_note_code/JavaSE/2.2/Demo1_Scale.java
+++
@@ -1,8 +0,0 @@
-class Demo1_Scale {
-	public static void main(String[] args) {
-		System.out.println(0b100);		//4		二进制
-		System.out.println(0100);		//64	八进制
-		System.out.println(100);		//100
-		System.out.println(0x100);		//256	十六进制
-	}
-}
